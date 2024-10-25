0c1eff84ac119339c5b59f4e32393480dcbe4cfa
Auto Logon <autologon@CSED.LAB>
2024-10-25 09:07:18 -0300

    Adicionando componente de listagem de produtos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProdutoListar from './ProdutoListar';
 
 function App() {
   return (
     <div className="App">
       <p>Projeto em React</p>
+      <ProdutoListar />
     </div>
   );
 }
